2b19b8439ecd839e5bd860b88602345d21a1b784
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-06 11:11:52 -0500

    created a database to store stuff

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,24 @@
 get '/' do
-  # Look in app/views/index.erb
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @text = Statement.find(params[:id])
+  erb :get_cool_url
+end
+
+get '/:cool_url' do
+  @text = Statement.all
+  erb :post_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  Statement.create(params[:text])
+  redirect "/"
 end
